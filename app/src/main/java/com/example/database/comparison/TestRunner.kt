b00b1731026d604b2f545c52c6145d6c19298245
Administrator <freeraz@mail.ru>
2019-05-24 11:33:31 +0300

    Implemented average runtime calculation.

--- app/src/main/java/com/example/database/comparison/TestRunner.kt
+++ app/src/main/java/com/example/database/comparison/TestRunner.kt
@@ -5,15 +5,20 @@ class TestRunner(private val logger: Logger) {
     /**
      * Executes given code, measured its running time and log it.
      */
-    fun run(identification: String, func: () -> Unit) {
-        // TODO System or SystemClock
-        val startTime = System.currentTimeMillis()
+    fun run(identification: String, runs: Int = 1, func: () -> Unit) {
+        val runTimes = LongArray(runs)
 
-        // Execute given code.
-        func()
+        for (i in 0 until runs) {
+            // TODO System or SystemClock
+            val startTime = System.currentTimeMillis()
+            // Execute given code.
+            func()
+            runTimes[i] = System.currentTimeMillis() - startTime
+        }
 
-        val finishTime = System.currentTimeMillis()
-        logger.log(identification, finishTime - startTime)
+        val averageRunTime = runTimes.sum()/runs
+
+        logger.log(identification, averageRunTime)
     }
 
 }
